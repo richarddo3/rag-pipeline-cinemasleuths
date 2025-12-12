6b2185b3e51eab6baeec60a1ddeebaed330d655c
Richard Do <msq2zu@virginia.edu>
2025-12-12 00:28:54 -0500

    Update embeddings.py

--- rag_pipeline/embeddings.py
+++ rag_pipeline/embeddings.py
@@ -1,1 +1,9 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 
+def get_embedding_model(model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    """
+    Loads a HuggingFace embedding model for use in vector stores.
+    Returns a LangChain-compatible embedding function.
+    """
+    embedder = HuggingFaceEmbeddings(model_name=model_name)
+    return embedder
