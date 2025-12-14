6e5f883767e32ef7a2a2bb28cffd9c609808a37f
Richard Do <msq2zu@virginia.edu>
2025-12-14 13:23:02 -0500

    Rename method and update docstring in Retriever class
    Renamed method from 'get_relevant_docs' to 'retrieve' and updated its docstring.

--- rag_pipeline/retriever.py
+++ rag_pipeline/retriever.py
@@ -1,16 +1,28 @@
 from rag_pipeline.vector_store import search_faiss
 
+
 class Retriever:
+    """
+    Thin wrapper around FAISS search that stores:
+        - index
+        - embeddings matrix
+        - metadata list
+    """
+
     def __init__(self, index, embeddings, metadata):
         self.index = index
         self.embeddings = embeddings
         self.metadata = metadata
 
-    def get_relevant_docs(self, query, k=5):
-        return search_faiss(
+    def retrieve(self, query, k=5):
+        """
+        Run similarity search and return top-k chunks + metadata
+        """
+        results = search_faiss(
             self.index,
             self.embeddings,
             self.metadata,
             query,
-            k,
+            k=k
         )
+        return results
