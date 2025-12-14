d79c646e2fac7d07c59c0e9f47288f4e3a3b57e6
Richard Do <msq2zu@virginia.edu>
2025-12-14 16:29:04 -0500

    Update embeddings.py

--- rag_pipeline/embeddings.py
+++ rag_pipeline/embeddings.py
@@ -2,13 +2,12 @@ from sentence_transformers import SentenceTransformer
 
 _model = None
 
-def load_embedding_model():
+def get_embedding_model():
     global _model
     if _model is None:
         _model = SentenceTransformer("BAAI/bge-small-en-v1.5")
     return _model
 
-
 def embed_texts(texts):
-    model = load_embedding_model()
+    model = get_embedding_model()
     return model.encode(texts, convert_to_numpy=True)
