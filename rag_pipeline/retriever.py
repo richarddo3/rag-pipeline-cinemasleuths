9137de039e4e186d90201732f5f152bea78d19a2
Richard Do <msq2zu@virginia.edu>
2025-12-14 12:42:59 -0500

    Update retriever.py

--- rag_pipeline/retriever.py
+++ rag_pipeline/retriever.py
@@ -9,7 +9,8 @@ class Retriever:
 
     def get_relevant_docs(self, query, k=5):
         """
-        Return top-k most relevant document metadata using FAISS search.
+        Returns the top-k most relevant document metadata entries
+        using FAISS similarity search.
         """
         return search_faiss(
             self.index,
