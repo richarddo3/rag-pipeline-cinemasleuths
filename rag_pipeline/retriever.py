43a7e4269f74791d0786c5dcac642de32d7918ab
Richard Do <msq2zu@virginia.edu>
2025-12-14 12:33:02 -0500

    Implement Retriever class with FAISS search

--- rag_pipeline/retriever.py
+++ rag_pipeline/retriever.py
@@ -1,1 +1,23 @@
+from rag_pipeline.vector_store import search_faiss
 
+class Retriever:
+    def __init__(self, index, embeddings, metadata):
+        self.index = index
+        self.embeddings = embeddings
+        self.metadata = metadata
+
+    def get_relevant_docs(self, query, k=5):
+        """
+        Returns FAISS search results: a list of dictionaries:
+        {
+            "distance": ...,
+            "metadata": {...}
+        }
+        """
+        return search_faiss(
+            self.index,
+            self.embeddings,
+            self.metadata,
+            query,
+            k
+        )
